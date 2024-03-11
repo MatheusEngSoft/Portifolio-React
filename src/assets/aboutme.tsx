487a08cf372e67aeca7448500dc599b94316b75a
MatheusEngSoft <matheus.engsof@gmail.com>
2024-03-11 19:42:24 -0300

    testando de nv

--- src/assets/aboutme.tsx
+++ src/assets/aboutme.tsx
@@ -3,7 +3,8 @@ import TimeLine from "./components/timeline"
 const events = [
   { data: '2023', description: 'comecei minha faculdade de engenharia de software'},
   { data: '2024', description: 'comecei a estudar desenvolvimento web pela DNC'},
-  { data:'2024', description: 'Testando'}
+  { data:'2024', description: 'Testando'},
+  {data:'2024', description: 'Testando novamente'}
 ]
 
 export default function AboutMe(){
